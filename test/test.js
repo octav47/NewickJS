abdd1f77f0af70d19856736fa8f6301bd1294d1b
tribunskiy <forty-7@yandex.ru>
2016-08-02 09:22:26 +0300

    added test for map method

--- test/test.js
+++ test/test.js
@@ -13,13 +13,16 @@ var assert = function (x) {
         console.log('F');
     }
 };
+// init
 assert(tree.name == 'F');
 assert(tree.branchset.length == 3);
 assert(tree.branchset[0].name == 'A');
 
+// serialize
 var serializedTree = Newick.serialize(tree);
 assert(serializedTree === treeString.replace(/\s+/g, ''));
 
+// normalize
 var normalizedDataTest = {
     b: 5.2631578947368425,
     d: 21.05263157894737,
@@ -42,3 +45,34 @@ for (var i in normalizedData) {
 }
 
 assert(normalizedDataTestResult);
+
+// map
+(function () {
+    var tree = Newick.parse(treeString);
+
+    var mappedDataTest = {
+        'A mapped': 1,
+        'B mapped': 2,
+        'E mapped': 5,
+        'C mapped': 3,
+        'D mapped': 4
+    };
+
+
+    tree = Newick.map(tree, function (e) {
+        e.length *= 10;
+        e.name += ' mapped';
+        return e;
+    });
+    var mappedDataTestResult = true;
+    var treeData = Newick.dfs(tree);
+    for (var i in treeData) {
+        if (treeData.hasOwnProperty(i)) {
+            if (treeData[i] - mappedDataTest[i] > 1e-10) {
+                mappedDataTestResult = false;
+                break;
+            }
+        }
+    }
+    assert(mappedDataTestResult);
+})();
