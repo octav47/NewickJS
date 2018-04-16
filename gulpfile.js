2df316d38cc5a9db5be2530ea4e7b97ab2bfadd3
Cyrus <forty-7@yandex.ru>
2018-04-16 12:32:28 +0300

    Newick 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,52 @@
-'use strict';
+'use strict'
 
-let gulp = require('gulp');
-let uglify = require('gulp-uglify');
-let rename = require('gulp-rename');
-let rimraf = require('rimraf');
+let gulp = require('gulp')
+let babel = require('gulp-babel')
+let uglify = require('gulp-uglify')
+let rename = require('gulp-rename')
+let concat = require('gulp-concat')
+let browserify = require('browserify')
+let babelify = require('babelify')
+let rimraf = require('rimraf')
 
 let path = {
-    dist: 'dist/'
-};
+    root: './',
+    src: './src/',
+    dist: 'dist/',
+    entryPoint: './src/index.js'
+}
 
 gulp.task('clean', function (cb) {
-    rimraf(path.dist, cb);
-});
+    rimraf(path.dist, cb)
+})
 
 gulp.task('dist', () => {
-    gulp.src('index.js')
-        .pipe(uglify())
-        .pipe(rename('newick.min.js'))
-        .pipe(gulp.dest(path.dist))
-});
+    gulp.src(path.entryPoint)
+        .pipe(babel({
+            "presets": ["env"],
+            "plugins": ["transform-class-properties"]
+        }))
+        .pipe(gulp.dest('dist'))
+})
 
-gulp.task('default', ['clean', 'dist']);
+gulp.task('make', () => browserify()
+    .transform(babelify.configure({
+        presets: ['es2015', 'stage-0']
+    }))
+    .require(path.entryPoint, { entry: true })
+    .bundle()
+    .pipe(gulp.dest(path.root)))
+
+gulp.task('js', function(){
+    return browserify({
+        entries: [path.entryPoint]
+    })
+        .transform(babelify.configure({
+            "presets": ["env"],
+            "plugins": ["transform-class-properties"]
+        }))
+        .bundle()
+        .pipe(gulp.dest(path.dist))
+})
+
+gulp.task('default', ['clean', 'dist'])
